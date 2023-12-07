bcb77cc40c7d99746f0a62c78f5a74feea371ca8
Shakhzod <96968478+shaxa98@users.noreply.github.com>
2023-12-07 20:17:39 +0500

    okok

---
+++ move-app-react/src/components/Body.jsx
@@ -0,0 +1,309 @@
+const BodyComponents = () => {
+  return (
+    <>
+      <div className="body">
+        <div className="body-1">
+          <div className="body-1-1">
+            <div className="body-1-1-1">
+              <div className="body-1-1-1-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="100%"
+                  height="100%"
+                  fill="currentColor"
+                  color="white"
+                  className="bi bi-house-door"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z" />
+                </svg>
+              </div>
+              <div className="body-1-1-1-2">
+                <h1>Home</h1>
+              </div>
+            </div>
+            <div className="body-1-1-1">
+              <div className="body-1-1-1-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="100%"
+                  height="100%"
+                  color="white"
+                  fill="currentColor"
+                  className="bi bi-search"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
+              </div>
+              <div className="body-1-1-1-2">
+                <h1>Search</h1>
+              </div>
+            </div>
+            <div className="body-1-1-1">
+              <div className="body-1-1-1-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="100%"
+                  height="100%"
+                  color="white"
+                  fill="currentColor"
+                  className="bi bi-collection"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2.5 3.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm2-2a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zm1.5.5A.5.5 0 0 1 1 13V6a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13z" />
+                </svg>
+              </div>
+              <div className="body-1-1-1-2">
+                <h1>Your Library</h1>
+              </div>
+            </div>
+          </div>
+          <div className="body-1-2">
+            <div className="body-1-2-1">
+              <div className="body-1-2-1-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="100%"
+                  height="100%"
+                  fill="currentColor"
+                  color="white"
+                  className="bi bi-plus-square-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z" />
+                </svg>
+              </div>
+              <div className="body-1-2-1-2">
+                <h2>Create Playlist</h2>
+              </div>
+            </div>
+            <div className="body-1-2-1">
+              <div className="body-1-2-1-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="100%"
+                  height="100%"
+                  color="white"
+                  fill="currentColor"
+                  className="bi bi-heart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                  />
+                </svg>
+              </div>
+              <div className="body-1-2-1-2">
+                <h2>Liked Songs</h2>
+              </div>
+            </div>
+          </div>
+          <div className="body-1-3">
+            <div>
+              <p>Legal</p>
+
+              <p>Privacy Center</p>
+            </div>
+            <div>
+              <p>Privacy Policy</p>
+              <p>Cookies</p>
+            </div>
+            <div>
+              <p>About Ads</p>
+            </div>
+            <div>
+              <p>Cookies</p>
+            </div>
+          </div>
+          <div className="body-1-4">
+            <div>
+              <img src="./02.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="body-2">
+          <div className="body-2-1">
+            <div>
+              <h1>Focus</h1>
+            </div>
+            <div>
+              <h2>Show all</h2>
+            </div>
+          </div>
+          <div className="body-2-2">
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="body-2-3">
+            <div>
+              <h1>Sopotify Playlist</h1>
+            </div>
+            <div>
+              <h2>Show all</h2>
+            </div>
+          </div>
+          <div className="body-2-4">
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="img">
+                <img src="./03.png" width="100%" height="100%" alt="" />
+              </div>
+              <div className="about-move">
+                <div className="card-title">
+                  <h1>Peaceful Piano</h1>
+                </div>
+                <div className="card-commit">
+                  <p>Relex end induluge with beuatiful piano pieces</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BodyComponents;
